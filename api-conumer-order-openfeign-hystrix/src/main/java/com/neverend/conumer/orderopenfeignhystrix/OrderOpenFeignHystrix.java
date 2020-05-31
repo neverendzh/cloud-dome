3f61da91c259b89e4c85a6997e415af167fe7349
zcj <neverend_zh@163.com>
2020-05-31 18:01:00 +0800

    服务熔断hystrix 监控页面搭建

--- api-conumer-order-openfeign-hystrix/src/main/java/com/neverend/conumer/orderopenfeignhystrix/OrderOpenFeignHystrix.java
+++ api-conumer-order-openfeign-hystrix/src/main/java/com/neverend/conumer/orderopenfeignhystrix/OrderOpenFeignHystrix.java
@@ -1,14 +1,30 @@
 package com.neverend.conumer.orderopenfeignhystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrix
+@EnableCircuitBreaker
 public class OrderOpenFeignHystrix {  public static void main(String[] args) {
     SpringApplication.run(OrderOpenFeignHystrix.class,args);
 }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+
+    }
 }
--- api-provider-payment-hystrix/src/main/java/com/neverend/payment/hystrix/PaymentHystrix.java
+++ api-provider-payment-hystrix/src/main/java/com/neverend/payment/hystrix/PaymentHystrix.java
@@ -1,13 +1,35 @@
 package com.neverend.payment.hystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 public class PaymentHystrix {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentHystrix.class,args);
+        SpringApplication.run(PaymentHystrix.class, args);
     }
+
+    /**
+     * 服务监控配置，与服务容错本身无关，springcloud升级后的坑，
+     * servletRegistrationBean 因为springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目里边配置如下servler就可以另
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+
+    }
+
+
 }
