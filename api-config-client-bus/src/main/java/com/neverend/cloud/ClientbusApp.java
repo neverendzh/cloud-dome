bebbe7aba3cb7f760dfa318ce931a898df83056d
zcj <neverend_zh@163.com>
2020-06-13 23:18:25 +0800

    引入消息总线，实现集群通知动态刷新

---
+++ api-config-client-bus/src/main/java/com/neverend/cloud/ClientbusApp.java
@@ -0,0 +1,14 @@
+package com.neverend.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientbusApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientbusApp.class,args);
+    }
+}
